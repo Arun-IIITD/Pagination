635c1527f9c14cfc319916768b7258e54d3be8fe
Arun Yadav <ay20552291@gmail.com>
2025-09-21 18:43:00 +0530

    second

---
+++ src/pagin.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+
+const Pagin = () => {
+    const [employees, setEmployees] = useState([]);
+    const [currPage, setCurrPage] = useState(1);
+    const emp_per_page  = 10
+
+ useEffect(() => {
+    const fetch_employee = async () => {
+      try {
+        const response = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        const data = await response.json();
+        setEmployees(data);
+      } catch (error) {
+        console.log("failed to fetch data.");
+      }
+    };
+
+    fetch_employee(); 
+  }, []); // it will run only once due to empty dependence array
+
+  const index_last_pg = currPage * emp_per_page;
+  const index_first_pg = index_last_pg - emp_per_page;
+  const curr_employee = employees.slice(index_first_pg,index_last_pg)
+  const total_page = Math.ceil(employees.length/ emp_per_page);
+
+
+
+
+
+
+    return (
+        <>
+
+        
+          <h2>Employee Data Table</h2>  
+          <table>
+               <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {curr_employee.map((emp) => (
+            <tr key={emp.id}>
+              <td>{emp.id}</td>
+              <td>{emp.name}</td>
+              <td>{emp.email}</td>
+              <td>{emp.role}</td>
+            </tr>
+          ))}
+        </tbody>
+
+          </table>
+        
+
+        <div>
+
+            <button
+            onClick={() => setCurrPage((p) => Math.max(p-1,1))}
+            
+            >
+             Previous
+            </button>
+
+            <span>
+                Page {currPage} of {total_page}
+            </span>
+
+             <button
+            onClick={() => setCurrPage((p) => Math.min(p+1,total_page))}
+            
+            >
+             Next
+            </button>
+
+
+
+
+        </div>
+
+
+        </>
+
+
+
+    )
+
+
+
+
+
+}
+
+export  default Pagin;
